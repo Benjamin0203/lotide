944988dffc280fddcfcabb5c981f70097eefe0cd
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-10-18 16:42:27 -0700

    eqArrays.js added

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = (actual, expected) => actual === expected ? console.log(`^ O ^ Assertion Passed: ${actual} === ${expected}`) : console.log(`T _ T Assertion Failed: ${actual} !== ${expected}`);
+
+const eqArrays = (arr1, arr2) => {
+  let copyOfArray = [];
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), false);
